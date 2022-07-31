38293047d17a7dffee87c4ce62639a5c62e293df
Marc <a0983204766@gmail.com>
2022-07-31 16:52:04 +0800

    add nav and homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Homepage from "./components/Homepage";
 
 const App = () => {
-  return <div>App12</div>;
+  return (
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/Nav.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">首頁</Link>
+          </li>
+          <li>
+            <Link to="/register">註冊</Link>
+          </li>
+          <li>
+            <Link to="login">登入</Link>
+          </li>
+          <li>
+            <Link to="/">登出</Link>
+          </li>
+          <li>
+            <Link to="profile">個人頁面</Link>
+          </li>
+          <li>
+            <Link to="postcourses">新增課程</Link>
+          </li>
+          <li>
+            <Link to="enroll">註冊課程</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
